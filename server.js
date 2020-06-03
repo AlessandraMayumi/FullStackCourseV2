0ede44ac619a60b8d3cc1180c3cb19bdb83e5903
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-06-02 22:40:46 -0300

    Finished Author Routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index.js')
 const authorRouter = require('./routes/authors.js')
@@ -15,6 +17,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 // sending data via url to the server
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
