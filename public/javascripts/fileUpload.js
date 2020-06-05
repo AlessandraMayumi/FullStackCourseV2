f5d043501eb5c7051de3c881918b9900a9081893
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-06-05 18:08:58 -0300

    css

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,6 +1,30 @@
-FilePond.registerPlugin(
-    FilePondPluginFileEncode,
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize
-)
-FilePond.parse(document.body)
+const rootStyle = window.getComputedStyle (document.documentElement)
+
+if (rootStyle.getPropertyValue('--book-cover-width-large') != null) {
+    read()
+} else {
+    document
+        .getElementById('main-css')
+        .addEventListener('load', read())
+}
+
+function read() {
+    const coverWidth = parseFloat(rootStyle.getPropertyValue('--book-cover-width-large'))
+    const coverRatio = parseFloat(1 / rootStyle.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverRatio
+
+    FilePond.registerPlugin (
+        FilePondPluginFileEncode,
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize
+    )
+
+    FilePond.setOptions ({
+        stylePanelAspectRatio:coverRatio,
+        imageResizeTargetWidth:coverWidth,
+        imageResizeTargetHeight:coverHeight
+    })
+
+    FilePond.parse(document.body)
+}
+
